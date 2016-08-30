19c1b20d5a7a2d631b35ff018e796e71b2bb6a8d
lifeng1992 <15823904221@163.com>
2016-08-30 10:09:25 +0800

    two

--- demo2.js
+++ demo2.js
@@ -86,9 +86,6 @@ map.set("age", 24);
 for(let [key, value] of map){
     console.log("key ------"+ key + "--------val:  "+value);
 }
-
 //输入模块的指定方法
-
 //加载模块时，往往需要指定输入那些方法。解构赋值使得输入语句非常清晰。
-
 //const { SourceMapConsumer, SourceNode } = require("source-map");
---
+++ demo3.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Administrator on 2016/8/24.
+ */
+var  a = "离a";
+console.log(a.length);
+console.log(a.charAt(0));
+console.log(a.charCodeAt(0));
+console.log(a.codePointAt(0));
+console.log(a.charCodeAt(0).toString());
+console.log(a.codePointAt(0).toString());
+
+for(let char of a){
+    console.log(char);
+}
+
+function is32Bit(c) {
+    return c.codePointAt(0) > 0xFFFF;
+}
+console.log(is32Bit("我"));
+console.log(is32Bit("a"));
+
+var s = 'Hello world!';
+
+console.log(s.startsWith('Hello'));// true
+console.log(s.endsWith('!'));// true
+console.log(s.includes('o'));// true
+//如果repeat的参数是负数或者Infinity，会报错。
+var s2 = s.repeat(2);
+console.log(s2);
+
+// 字符串中嵌入变量
+var name = "Bob", time = "today";
+function getName(){
+    return "lee";
+}
+console.log(`Hello ${name}, how are you ${time}?`);
+console.log('hi,my name is ${getName()}');
