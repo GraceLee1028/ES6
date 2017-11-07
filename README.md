7a80d3ea9613ec237d854a7c487018b43f1df207
1924458755@qq.com <1924458755@qq.com>
2017-11-07 16:16:33 +0800

    es6 settings finish

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ES6
 ES6的学习示例
+webstorm es6 配置
+①、npm install -g babel-cli
+②、npm install --save-dev babel-preset-es2015
+③、根路径新建.babelrc文件，内容如下
+  {
+    "presets": [
+      "es2015"
+    ]
+  }
+④、将javascript设置为es6
--- const-compiled.js
+++ const-compiled.js
@@ -1,9 +1,11 @@
 /**
  * Created by Administrator on 2016/8/5.
  */
-    "use strict";
+"use strict";
 //const声明一个只读的常量。一旦声明，常量的值就不能改变
-const PI = 3.1415;
-let a = "hello";
+
+var PI = 3.1415;
+var a = "hello";
 console.log(a);
 
+//# sourceMappingURL=const-compiled.js.map
